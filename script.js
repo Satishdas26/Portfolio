bda8bfa3b31c48e9b4f9e3db8782719a7f6134d1
Satish Ranjan Das <satishdas@Satishs-MacBook-Air.local>
2022-07-16 20:21:49 +0530

    update

--- script.js
+++ script.js
@@ -1,71 +1,83 @@
 function validate(){
-    const name = document.getElementById("name").value;
-    const mobile=document.getElementById("mobile").value;
-    const email=document.getElementById("email").value;
-    const term=document.getElementById("term").value;
-    const sugst=document.getElementById("sugesstion").value;
-    let error=false;
-    //Name
-    if(name ===""){
-        document.getElementById("name_error").innerHTML="Name is required.";
-        error=true;
+    const name = document.getElementById("name").value
+    const mobile = document.getElementById("mobile").value
+    const email = document.getElementById("email").value
+    const term = document.getElementById("term")
+
+
+    let error= false
+    if(name===""){
+        // console.log('name is required')
+        document.getElementById("name_error").innerHTML = "Name is required"
+        error = true
+    }
+    else if(!name.match(/[a-z]/)){
+        document.getElementById("name_error").innerHTML = "name must have one lower case letter"
+        error = true
+    }
+    else if(!name.match(/[A-Z]/)){
+        document.getElementById("name_error").innerHTML = "name must have one Upper case letter"
+        error = true
+    }
+    else if(name.match(/[!@#$%^&]/) && (name.match(/[0-9]/))){
+        document.getElementById("name_error").innerHTML = "Name doesnot contain special character and numeric value"
+        error = true
+    }
+    else if(name.match(/[!@#$%^&]/)){
+        document.getElementById("name_error").innerHTML = "name doesnot contain special character"
+        error = true
+    }
+    else if(name.match(/[0-9]/)){
+        document.getElementById("name_error").innerHTML = "name doesnot contain numeric value"
+        error = true
     }
     else{
-        document.getElementById("name_error").innerHTML="";
+        document.getElementById("name_error").innerHTML = ""
     }
-    
-    //Mobile number
-    if(mobile ===""){
-        document.getElementById("mobile_error").innerHTML="Enter mobile number.";
-        error=true;
+    //Mobile Validation
+    if(mobile === ""){
+        document.getElementById("mobile_error").innerHTML = "mobile number is required"
+        error = true
     }
-    else if(mobile.length != 10 || isNaN(mobile)){
-        document.getElementById("mobile_error").innerHTML="Please Enter a valid 10 digit mobile number.";
-        error=true;
-    }
-    else{
-        document.getElementById("mobile_error").innerHTML="";
-    }
-    //Email
-    let atpos=email.indexOf("@");
-    let atposs=email.lastIndexOf("@");
-    let dotpos=email.lastIndexOf(".");
-    if(email === ""){
-        document.getElementById("email_error").innerHTML="Email is required.";
-        error=true;
-    }
-    else if(atpos<=0||dotpos<=0||[dotpos-atpos]<=4||dotpos==email.length -1|| atpos!=atposs){
-        document.getElementById("email_error").innerHTML="Please Enter a valid Email.";
-        error=true;
+    else if(mobile.length !== 10 || isNaN(mobile)){
+        document.getElementById("mobile_error").innerHTML = "Please enter a 10 digit valid mobile number"
+        error = true
     }
     else{
-        document.getElementById("email_error").innerHTML="";
+        document.getElementById("mobile_error").innerHTML = ""
     }
-    
-    //Sugesstion
-    if(sugst ===""){
-        document.getElementById("sugesstion_error").innerHTML="Please type in your sugesstions before submitting";
-        error=true;
+    //Email Validation
+
+    let atPos = email.indexOf('@')
+    let dotPos = email.lastIndexOf('.')
+
+    if(email===""){
+        document.getElementById("email_error").innerHTML = "email is required"
+        error = true
+    }
+    else if(atPos <=0 || dotPos <= 0 || (dotPos - atPos) <= 4 || dotPos == email.length-1 ){
+        document.getElementById("email_error").innerHTML = "Please enter a valid email id"
+        error = true
     }
     else{
-        document.getElementById("sugesstion_error").innerHTML="";
+        document.getElementById("email_error").innerHTML = ""
+    }
+    //Check box
+
+    if(!term.checked){
+        document.getElementById("term_error").innerHTML = "Please agree to the terms and condition"
+        error = true
+    }
+    else{
+        document.getElementById("term_error").innerHTML = ""
     }
 
-    //Term
-    if(!term.checked){
-        document.getElementById("term_error").innerHTML="Please agree to the term and condition.";
-        error=true;
-    }
-    else{
-        document.getElementById("term_error").innerHTML="";
-    }
+    
 
     if(error){
-        return false;
+        return false
     }
     else{
-        return true;
+        return true
     }
-
-    
 }
